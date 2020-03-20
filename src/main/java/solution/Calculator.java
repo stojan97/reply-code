9162014959a183d54daa6f95e2c9f394f0aed3da
stojan97 <stoki97.basketball@hotmail.com>
2020-03-20 10:19:55 +0100

    format Calculator

--- src/main/java/solution/Calculator.java
+++ src/main/java/solution/Calculator.java
@@ -4,9 +4,9 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-public class Calculator {
+public final class Calculator {
 
-  static long wp(Replyer a, Replyer b) {
+  public static long wp(Replyer a, Replyer b) {
 
     Set<String> sa = new HashSet<>(a.skills);
     Set<String> sb = new HashSet<>(b.skills);
@@ -17,10 +17,11 @@ public class Calculator {
 
     sa.retainAll(sb);
     long intersection = sa.size();
+
     return intersection * (union - intersection);
   }
 
-  static long bp(Replyer a, Replyer b) {
+  public static long bp(Replyer a, Replyer b) {
 
     if (Objects.equals(a.company, b.company)) {
       return a.bonusPoints * b.bonusPoints;
